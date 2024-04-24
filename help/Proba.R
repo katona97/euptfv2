0b34e7b470cd65cd51a3fd5d2423fafd82bfad46
Katona Máté <katona97@gmail.com>
2024-04-24 12:05:17 +0200

    FC02 pred

--- help/Proba.R
+++ help/Proba.R
@@ -9,3 +9,25 @@ new_data <- read_csv2("help/Ajka.csv")
 
 # check available predictors in your dataset
 names(new_data)
+
+
+# find which PTFs are recommended for given set of available predictors
+list_PTFs <- read_csv2("suggested_PTFs/list_of_final_PTFs.csv")
+View(list_PTFs)
+# available predictors in "new_data": USSAND USSILT USCLAY DEPTH_M OC BD CACO3 PH_H2O CEC
+# row 32 includes recommended PTFs
+
+
+### a) predict response without uncertainty ----
+## prediction of field capacity (FC) with PTF07
+
+# load prediction algorithm
+load("suggested_PTFs/FC_EUHYDI/FC_PTF02.rdata")
+FC <- predict(FC_PTF02,
+              data=new_data,
+              type = "response",
+              num.threads = detectCores()-1)
+FC <- FC$predictions
+
+# add predicted values to the input dataset
+new_data_pred2 <- cbind(new_data, FC)
